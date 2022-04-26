a2e0ca182a04db1d2f8e4fb0ae01c16896edecda
Shrivastava-Aditya <Aditya@github.com>
2022-04-26 14:16:27 +0530

    apr 26,2022

--- algo/Java/Searching/BinarySearch.java
+++ algo/Java/Searching/BinarySearch.java
@@ -1,5 +1,24 @@
 package algo.Java.Searching;
 
 public class BinarySearch {
-    
+    public static boolean search(int[] arr,int target){
+        int l = 0, r = arr.length;
+        int m;
+
+        if(!isSorted(arr)) return false;
+
+        while(l<=r){
+            m = l + (r - l) /2;
+            if(arr[m] == target) return true;
+            else if(arr[m] > target) r = m - 1;
+            else if(arr[m] < target) l = m + 1;
+        }
+        return false;
+    }
+    public static boolean isSorted(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]) return false;
+        }
+        return true;
+    }
 }
--- algo/Java/Searching/Searches.java
+++ algo/Java/Searching/Searches.java
@@ -2,6 +2,7 @@ package algo.Java.Searching;
 
 public class Searches{
     public static void main(String[] args) {
-        
+        // BinarySearch bs = new BinarySearch();
+        System.out.println(BinarySearch.search(new int[]{1,5,7,84,42,100,134,1942}, 84));
     }
 }
